c4042eec76c264e5d0afcc2ceb8afb96bbddae06
Saroj Budhathoki <sarojbudhathoki@Sarojs-MacBook-Pro.local>
2026-02-14 16:21:36 +0545

    added section

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+// gsap code goes here
+
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+
+SplitText.create(".about-text",{
+    type: 'chars',
+    autoSplit: true,
+    onSplit: (self) => {
+        return gsap.from{
+            opacity: 25,
+
+            scrollTrigger: {
+                scrub: true;
+                trigger: "about-section"
+                start: '20% 100%'
+                end: "100% 100%"
+            }
+        }
+    }
+})
+
 console.log("Hello there");
